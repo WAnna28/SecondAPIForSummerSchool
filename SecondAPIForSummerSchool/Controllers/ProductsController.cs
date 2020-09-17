4cd8e13ae434c7898e83910e9938594632b2f082
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2020-09-17 15:02:51 +0400

    Add project files.

---
+++ SecondAPIForSummerSchool/Controllers/ProductsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecondAPIForSummerSchool.Models;
+
+namespace SecondAPIForSummerSchool.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            // The <context class>.Database.EnsureData() call seeds the data, if that hasn't happened before.
+            _context.Database.EnsureCreated();
+        }
+
+        //Example 1
+        //[HttpGet]
+        //public string GetProducts()
+        //{
+        //    return "OK. SummerSchool";
+        //}
+
+        // Example 2: returning a list of items
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        // Example 3: Using IActionResult
+        // The okay helper method returns, HTTP status code of 200, 
+        // and you may recall that, okay, 
+        // is the text description of that HTTP status code, 
+        // plus the actual product in JSON format.
+        //[HttpGet]
+        //public IActionResult GetAllProducts()
+        //{
+        //    var products = _context.Products.ToList();
+        //    return Ok(products);
+        //}
+
+        // Example 4: Returning an Item
+        //[HttpGet, Route("{id}")]
+        //public IActionResult GetAllProducts(int id)
+        //{
+        //    var product = _context.Products.Find(id);
+        //    return Ok(product);
+        //}
+
+        //Example 5: Handling errors
+        //[HttpGet, Route("{id:int}")]
+        //public IActionResult GetAllProducts2(int id)
+        //{
+        //    var product = _context.Products.Find(id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(product);
+        //}
+
+        // Example 6: the resource here, this URI doesn't exist.
+        [HttpGet, Route("{id:int}")]
+        public IActionResult GetAllProducts(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // Example 7: Adding an item with POST
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct([FromBody] Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("PostProduct", new { id = product.Id }, product);
+        }
+
+        // Example 8: Updating an item with PUT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct([FromRoute] int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Products.Find(id) == null)
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
+
+        // Example 8: Deleting an item with DELETE
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
---
+++ SecondAPIForSummerSchool/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondAPIForSummerSchool.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Category1" },
+                new Category { Id = 2, Name = "Category2" },
+                new Category { Id = 3, Name = "Category3" },
+                new Category { Id = 4, Name = "Category4" },
+                new Category { Id = 5, Name = "Category5" },
+                new Category { Id = 6, Name = "Category6" },
+                new Category { Id = 7, Name = "Category7" }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, CategoryId = 1, Name = "Product1", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 2, CategoryId = 1, Name = "Product2", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 3, CategoryId = 1, Name = "Product3", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 4, CategoryId = 1, Name = "Product4", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 5, CategoryId = 1, Name = "Product5", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 6, CategoryId = 1, Name = "Product6", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 7, CategoryId = 1, Name = "Product7", Sku = "Sku1", Description = "Desc1", IsAvailable = true, Price = 2020 },
+                new Product { Id = 8, CategoryId = 2, Name = "Product8", Sku = "Sku2", Description = "Desc2", IsAvailable = true, Price = 2020 },
+                new Product { Id = 9, CategoryId = 2, Name = "Product9", Sku = "Sku2", Description = "Desc2", IsAvailable = true, Price = 2020 },
+                new Product { Id = 10, CategoryId = 2, Name = "Product10", Sku = "Sku2", Description = "Desc2", IsAvailable = true, Price = 2020 },
+                new Product { Id = 11, CategoryId = 3, Name = "Product10", Sku = "Sku3", Description = "Desc3", IsAvailable = true, Price = 2020 },
+                new Product { Id = 12, CategoryId = 3, Name = "Product12", Sku = "Sku3", Description = "Desc3", IsAvailable = true, Price = 2020 },
+                new Product { Id = 13, CategoryId = 4, Name = "Product13", Sku = "Sku4", Description = "Desc4", IsAvailable = true, Price = 2020 },
+                new Product { Id = 14, CategoryId = 4, Name = "Product14", Sku = "Sku4", Description = "Desc4", IsAvailable = true, Price = 2020 },
+                new Product { Id = 15, CategoryId = 4, Name = "Product15", Sku = "Sku4", Description = "Desc4", IsAvailable = true, Price = 2020 },
+                new Product { Id = 16, CategoryId = 4, Name = "Product16", Sku = "Sku4", Description = "Desc4", IsAvailable = true, Price = 2020 },
+                new Product { Id = 17, CategoryId = 5, Name = "Product17", Sku = "Sku5", Description = "Desc5", IsAvailable = true, Price = 2020 },
+                new Product { Id = 18, CategoryId = 5, Name = "Product18", Sku = "Sku5", Description = "Desc5", IsAvailable = true, Price = 2020 },
+                new Product { Id = 19, CategoryId = 5, Name = "Product19", Sku = "Sku51", Description = "Desc5", IsAvailable = true, Price = 2020 },
+                new Product { Id = 20, CategoryId = 5, Name = "Product20", Sku = "Sku5", Description = "Desc5", IsAvailable = true, Price = 2020 },
+                new Product { Id = 21, CategoryId = 5, Name = "Product21", Sku = "Sku5", Description = "Desc5", IsAvailable = true, Price = 2020 }
+            );
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Email = "user1@use1.user1" },
+                new User { Id = 2, Email = "user2@use2.user2" }
+             );
+        }
+    }
+}
